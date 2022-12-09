e2bf53c1fd0b68a53e0d42bef465bb685cb7babb
Bryanskiy <ivakin.kir@gmail.com>
2022-12-09 22:00:15 +0300

    [infra] init

---
+++ cmake/upd_tar_list.cmake
@@ -0,0 +1,6 @@
+macro(upd_tar_list NEW_TAR TARGETS)
+  # Append target name to targets list
+  # & propagate updated list to parent scope
+  list(APPEND ${TARGETS} ${NEW_TAR})
+  set(${TARGETS} ${${TARGETS}} PARENT_SCOPE)
+endmacro()
---
+++ main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <cstdint>
+#include <vector>
+
+#include "algorithms/sha-2.hh"
+
+int main() {
+
+}
