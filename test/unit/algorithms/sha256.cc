6f279dfc14705a27e612ad2ff276630ae37c30a3
Bryanskiy <ivakin.kir@gmail.com>
2022-12-09 22:12:33 +0300

    [algorithms] sha256

--- test/unit/algorithms/sha256.cc
+++ test/unit/algorithms/sha256.cc
@@ -1,3 +1,10 @@
 #include "test_header.hh"
+#include "algorithms/sha256.hh"
+
+TEST(algorithms, sha256)
+{
+    SHA256 sha256;
+    ASSERT_EQ(sha256(""), "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+}
 
 #include "test_footer.hh"
