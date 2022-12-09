2321b58557fefd4fa87393b82cf90e495bf69cc2
uslstenn <shurygin0171@gmail.com>
2022-12-09 22:54:00 +0300

    [sha_proj] minor fixies
    tuple ctor was fixed
    sha_size arg was added

--- driver.py
+++ driver.py
@@ -4,8 +4,9 @@ from collections import defaultdict
 from time import time
 
 class SHAWrapper():
-    def __init__(self, path : Path, sha_types : str()) -> None:
+    def __init__(self, path : Path, sha_size : int = None, sha_types : str = None) -> None:
         self.path = path
+        self.sha_size = sha_size
         self.sha_types = sha_types
         self.data = list()
         #
@@ -32,7 +33,7 @@ class SHAWrapper():
             #
             512 : list([tuple([hashlib.sha512, "sha512"]),              \
                         tuple([hashlib.sha3_512, "sha3_512"]),          \
-                        tuple(hashlib.blake2b, "blake2b")]),            \
+                        tuple([hashlib.blake2b, "blake2b"])]),            \
         })
         #
         self.collisions_stats = dict()
@@ -43,38 +44,44 @@ class SHAWrapper():
         return self._size2algo.get(size)
         #
     #
+    def run(self):
+        self.parse()
+        cur_shas = self._size2algo.get(self.sha_size)
+        #
+        for cur_sha in cur_shas:
+            func, name = cur_sha[0], cur_sha[1]
+            self.run_test(func, name)        
+        #
+    #
     def parse(self):
         with open(self.path) as src:
-            self.data(src.readline()) 
+            pwds = src.read().split('\n')
+            for pwd in pwds:
+                self.data.append(pwd)
         #
-    #s
-    def run_test(self, func, name : str, size : int):
+    #
+    def run_test(self, func, name : str):
+        collisions = defaultdict()
         collisions = defaultdict(lambda:0, collisions)
         #
         start = time()
         for data_it in self.data:
-            res_hash = func(data_it)
-            collisions[res_hash] += 1
+            res_hash = func(data_it.encode())
+            if name in ["shake_128", "shake_256"]:
+                collisions[res_hash.hexdigest(self.sha_size)] += 1
+            else: 
+                collisions[res_hash.hexdigest()] += 1
         #
         end = time()
         #
-        self.process_collisions(collisions, name, size)
-        self.time_stats[tuple([name, size])] = end - start
+        self.process_collisions(collisions, name)
+        self.time_stats[tuple([name, self.sha_size])] = end - start
         #
     #
-    def run(self):
-        self.parse()
-
-        for (size, cur_shas) in self._size2algo.items():
-            for cur_sha in cur_shas:
-                func, name = cur_sha[0], cur_sha[1]
-                self.run_test(func, name, size)        
-        #
-    #
-    def process_collisions(self, data : defaultdict, name : str, size : int):
+    def process_collisions(self, data : defaultdict, name : str):
     #
         collisions_num = 0
         for it in data.values():
             collisions_num += (it - 1)
             #
-        self.collisions_stats[tuple([name, size])] = collisions_num
+        self.collisions_stats[tuple([name, self.sha_size])] = collisions_num
