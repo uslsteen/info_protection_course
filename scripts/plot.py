d2538959ce66ea613ed88085ea7c12512c963e33
derzhavin3016 <derzhavin.andrej@yandex.ru>
2022-12-10 13:03:54 +0300

    Fix plot script

--- scripts/plot.py
+++ scripts/plot.py
@@ -5,34 +5,44 @@ import argparse
 import matplotlib.pyplot as plt
 import csv
 
-def parse(path_to_csv : Path) -> dict:
+
+def parse(path_to_csv: Path) -> dict:
     data = dict()
     #
-    with open(path_to_csv, newline='') as csvfile:
-        spamreader  = csv.reader(csvfile, delimiter = ',')
+    with open(path_to_csv, newline="") as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=",")
         #
         for row in spamreader:
-            data[row[0]] = row[1]
+            data[row[0]] = int(row[1])
     #
     return data
     #
+
+
 #
-def get_plot(data : dict, title):
-    plt.figure(figsize=(16,10))
-    plt.bar(list(data.keys()), data.values(), color ='g')
+def get_plot(data: dict, title):
+    plt.figure(figsize=(16, 10))
+    plt.bar(list(data.keys()), data.values(), color="g")
     plt.title(title)
     plt.show()
     #
+
+
 #
+TYPES = ["collisions", "time"]
+
+
 def main():
     parser = argparse.ArgumentParser(description="Tool to make plots")
-    parser.add_argument("--path_to_csv", type=str, help="Path to .csv sources")
-    parser.add_argument("--type", type=str, help="collisions/time")
+    parser.add_argument(
+        "--path-to-csv", type=Path, required=True, help="Path to .csv sources"
+    )
+    parser.add_argument("--type", choices=TYPES, required=True, help="collisions/time")
     #
     args = parser.parse_args()
     #
-    exec_path = Path.resolve(Path(__file__)).parent
-    src_path = exec_path.joinpath(args.path_to_csv)
+    exec_path = Path(__file__).resolve().parent
+    src_path = exec_path / args.path_to_csv
     plot_type = args.type
 
     title = "collisions statistics in birthday benchmark"
@@ -43,6 +53,8 @@ def main():
     #
     get_plot(data, title)
     #
+
+
 #
 if __name__ == "__main__":
     main()
